5a5c1db6ceb1eb52d88559c8605ccfea48a56191
suramyavns <sdidwania645@gmail.com>
2024-10-20 13:30:36 -0400

    static code analysis

--- server.py
+++ server.py
@@ -1,15 +1,25 @@
+'''
+This is the main server script for the application
+'''
+
 from flask import Flask,jsonify,request,render_template
-from EmotionDetection.emotion_detection import *
+from EmotionDetection import emotion_detection
 app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def root():
+    '''
+    Root path of this server
+    '''
     return render_template('index.html')
 
 @app.route('/emotionDetector',methods=['GET'])
-def emotionDetector():
+def emotion_detector():
+    '''
+    REST endpoint to get the emotion analysis for the query text
+    '''
     text = request.args.get('textToAnalyze')
-    emotion = emotion_detector(text)
+    emotion = emotion_detection.emotion_detector(text)
     if emotion['dominant_emotion']:
         return jsonify(emotion)
     return 'Invalid text! Please try again!\n'
